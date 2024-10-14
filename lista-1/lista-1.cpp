e4c2752c0f79adba4c20293d842d2eb173096bc3
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-10-14 21:16:25 +0200

    Update lista-1.cpp

--- lista-1/lista-1.cpp
+++ lista-1/lista-1.cpp
@@ -5,10 +5,11 @@
 int main()
 {
 	//Zadanie 1
-	auto comp = [](auto a, auto b) {return a < b; };
+	auto comp = [](auto a, auto b) { return a < b; };
 
 	std::cout << "Typ int: " << my_max(5, 6, comp) << "\n";
-	std::cout << "Typ float: " << my_max(5.5, 10.1, comp) << "\n\n";
+	std::cout << "Typ float: " << my_max(3.14f, 3.2f, comp) << "\n";
+	std::cout << "Typ double: " << my_max(3.14 , 3.2, comp) << "\n\n";
 
 
 	//Zadnie 2
@@ -35,20 +36,26 @@ int main()
 	vector.push_back(32);
 	vector.push_back(64);
 
+	std::cout << "Stworzenie nowego wektora poprzez dodawanie elementow, wektor = ";
+	print_vector(vector);
+
 	//usunięcie elementu
 	vector.pop_back();
+	std::cout << "Usuniecie ostatniego elementu, wektor = ";
+	print_vector(vector);
 
 	//odczytanie wartości
-	std::cout << vector[0] << "\n";
+	std::cout << "Odczytanie wartosci, wektor[0] = " << vector[0] << "\n";
 
 	//zmiana wartości
 	vector[0] = 1;
+	std::cout << "Zmiana wartosci, wektor[0] = " << vector[0] << "\n";
 
 	//typ danych wektora
 	std::cout << "Typ danych wektora: " << vector.data_type() << "\n";
 
 	//obliczenie ilocznu sklalarnego
-	std::cout << "Iloczyn skalarny: " << vector * vector_i;
+	std::cout << "Iloczyn skalarny wektora std:: i cpplab:: = " << vector_i * vector << "\n";
 
-	//std::cout << "vector size: " << vector.Size() << "\nvector capacity: " << vector.Capacity() << "\n";
+	//std::cout << "vector size: " << vector.size() << "\nvector capacity: " << vector.capacity() << "\n";
 }
