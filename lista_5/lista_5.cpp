3d71905db7b2e4c1686d275b1e9d00a85e54a62d
IgnacyZywalewski <127679558+IgnacyZywalewski@users.noreply.github.com>
2024-12-14 22:49:25 +0100

    Update lista_5.cpp

--- lista_5/lista_5.cpp
+++ lista_5/lista_5.cpp
@@ -7,14 +7,15 @@ int main()
 {
     //Zadanie 1
     BST<int> tree;
-    tree.insert(1);
+    tree.insert(8);
     tree.insert(10);
     tree.insert(4);
-    tree.insert(50);
-    tree.insert(2);
-    tree.insert(100);
+    tree.insert(9);
+    tree.insert(5);
+    tree.insert(3);
     tree.insert(7);
 
+    std::cout << "Drzewo binarne w kolejnosci: ";
     tree.print_in_order();
 
     std::cout << "\n";
@@ -24,8 +25,7 @@ int main()
     cpplab::unique_ptr<int> pointer(new int(1));
     std::cout << "wartosc ptr = " << *pointer << "\n";
 
-    if (pointer) 
-        std::cout << "ptr nie jest pusty\n";
+    if (pointer) std::cout << "ptr nie jest pusty\n";
 
     cpplab::unique_ptr<int> pointer2 = std::move(pointer);
 
@@ -43,10 +43,10 @@ int main()
 
 
     //Zadanie 3 
-    int N = 3;
-    int W = 50;
-    std::vector<int> prices = {60, 100, 120};
-    std::vector<int> weights = {10, 20, 30};
+    std::vector<int> prices = {1, 7, 11};
+    std::vector<int> weights = {1, 2, 3};
+    int N = prices.size();
+    int W = 5;
     int max_price = dratewka(N, W, prices, weights);
     std::cout << "Maksymalna cena = " << max_price << "\n";
     
